f1aad679af7fc877ec2103ce244d8109de1eef66
Jose Barrios <jose@barrios.io>
2017-11-16 16:57:19 -0800

    Update ssl-setup
    Because this is more generic, it requires some customization (directories, etc). Eventually, we'll take those variables as parameters on the commandline

--- redhat-ssl-setup.sh
+++ redhat-ssl-setup.sh
@@ -2,6 +2,8 @@ sudo yum install python;
 cd ..
 git clone https://github.com/letsencrypt/letsencrypt;
 cd letsencrypt/;
+
+#Replace directory [ec2-user/xxxx/public] with new app directory, and same goes for domains and subdomains
 ./certbot-auto certonly --webroot -w /home/ec2-user/barrios.io/public -d barrios.io -d www.barrios.io
 #Create syslinks to certificates in the server's public folder
 #Make sure port 80 (http) and 443 (https) forward to the respective http and https servers (usually 3000 and 4000)
