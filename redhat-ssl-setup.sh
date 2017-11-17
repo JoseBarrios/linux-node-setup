193bd2d96c531f95329e29f7a2dca30633ebc3d6
Jose Barrios <jose@cognilab.com>
2017-11-16 16:49:02 -0800

    Added ssl-setup bash script for RedHat Linux servers

---
+++ redhat-ssl-setup.sh
@@ -0,0 +1,6 @@
+sudo yum install python27-devel git;
+git clone https://github.com/letsencrypt/letsencrypt;
+cd letsencrypt/;
+./certbot-auto certonly --webroot -w /home/ec2-user/barrios.io/public -d barrios.io -d www.barrios.io
+#Create syslinks to certificates in the server's public folder
+#Make sure port 80 (http) and 443 (https) forward to the respective http and https servers (usually 3000 and 4000)
