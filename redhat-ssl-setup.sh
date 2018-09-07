155b7177d8ead52deac5f346ffa417ecc9bf0440
Jose Barrios <jose@barrios.io>
2018-09-06 21:40:52 -0700

    Generalized folder
    Changed folder path so it's cert, accessible at the root directory level

--- redhat-ssl-setup.sh
+++ redhat-ssl-setup.sh
@@ -3,7 +3,7 @@ cd ..
 git clone https://github.com/letsencrypt/letsencrypt;
 cd letsencrypt/;
 
-#Replace directory [ec2-user/xxxx/config] with new app directory, and same goes for domains and subdomains
-./certbot-auto certonly --webroot -w /home/ec2-user/barrios.io/config -d barrios.io -d www.barrios.io
+#Replace directory [ec2-user/certs] with new app directory, and same goes for domains and subdomains
+./certbot-auto certonly --webroot -w /home/ec2-user/certs -d barrios.io -d canary.barrios.io
 #Create syslinks to certificates in the server's public folder
 #Make sure port 80 (http) and 443 (https) forward to the respective http and https servers (usually 3000 and 4000)
