d5a5089cc7cb437157677bad7c9152c01dc88cca
Cloud User <ec2-user@ip-172-31-6-179.us-west-1.compute.internal>
2018-09-07 02:25:35 -0400

    Updated to use acme challenge

--- redhat-ssl-setup.sh
+++ redhat-ssl-setup.sh
@@ -4,6 +4,7 @@ git clone https://github.com/letsencrypt/letsencrypt;
 cd letsencrypt/;
 
 #Replace directory [ec2-user/certs] with new app directory, and same goes for domains and subdomains
-./certbot-auto certonly --webroot -w /home/ec2-user/certs -d barrios.io -d canary.barrios.io
+./certbot-auto certonly --webroot -w /home/ec2-user/app-node-template/public/ -d canary.barrios.io
+
 #Create syslinks to certificates in the server's public folder
 #Make sure port 80 (http) and 443 (https) forward to the respective http and https servers (usually 3000 and 4000)
