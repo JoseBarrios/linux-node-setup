fc8e859134bc44e96acedbe8621aaf60e9ee060c
Jose Barrios <jose@barrios.io>
2018-09-06 21:39:17 -0700

    CMake
    Added CMake dependency

--- redhat-setup.sh
+++ redhat-setup.sh
@@ -3,6 +3,7 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 
 #Now that we have the package, let's install it
 sudo yum -y install nodejs
+sudo yum install gcc-c++ make; 
 
 #Let's go back to the mail directory
 cd ..
