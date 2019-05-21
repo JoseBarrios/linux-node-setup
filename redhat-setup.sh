f1b47922466f0f02f3e5dd1857cdae6eb3089d50
Jose Barrios <github@barrios.io>
2019-05-20 17:22:10 -0700

    Added homebrew support
    Signed-off-by: Jose Barrios <github@barrios.io>

--- redhat-setup.sh
+++ redhat-setup.sh
@@ -1,9 +1,21 @@
+# Install Homebrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+
+sudo yum groupinstall 'Development Tools'
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+brew install gcc
+brew install vim
+brew postinstall perl
+brew install cmake
+
 #Install binary from source (in this case, Node v8)
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 
 #Now that we have the package, let's install it
 sudo yum -y install nodejs
-sudo yum install gcc-c++ make; 
+sudo yum install gcc-c++ make;
 
 #Let's go back to the mail directory
 cd ..
