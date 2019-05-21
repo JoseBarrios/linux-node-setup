a961531367e790327c3e613bee44e3d67fc37661
Jose Barrios <github@barrios.io>
2019-05-20 17:25:33 -0700

    Added iptables
    Signed-off-by: Jose Barrios <github@barrios.io>

--- redhat-setup.sh
+++ redhat-setup.sh
@@ -16,6 +16,7 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 #Now that we have the package, let's install it
 sudo yum -y install nodejs
 sudo yum install gcc-c++ make;
+sudo yum install iptables-services
 
 #Let's go back to the mail directory
 cd ..
