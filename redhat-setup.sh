a822a814f832c8203871e2aa36167f468c4ae86c
Jose Barrios <github@barrios.io>
2019-05-30 15:28:26 -0700

    Removing brew for now

--- redhat-setup.sh
+++ redhat-setup.sh
@@ -1,14 +1,14 @@
-# Install Homebrew
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+# TODO: Figure out a way to mitigate the Python versioning issue (multiple Python version cause issues in node_gyp)
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+#sudo yum groupinstall 'Development Tools'
+#echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
+#eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+#brew install gcc
+#brew install vim
+#brew postinstall perl
+#brew install cmake
 
-
-sudo yum groupinstall 'Development Tools'
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-brew install gcc
-brew install vim
-brew postinstall perl
-brew install cmake
+alias python="/usr/bin/env python3"
 
 #Install binary from source (in this case, Node v8)
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
