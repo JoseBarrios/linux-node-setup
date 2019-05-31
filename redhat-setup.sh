a91bafb96ae6397c6952a56fdd5372d26cf21773
Jose Barrios <github@barrios.io>
2019-05-30 17:38:27 -0700

    Ubuntu setup

--- redhat-setup.sh
+++ redhat-setup.sh
@@ -1,22 +1,5 @@
-# TODO: Figure out a way to mitigate the Python versioning issue (multiple Python version cause issues in node_gyp)
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-#sudo yum groupinstall 'Development Tools'
-#echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
-#eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-#brew install gcc
-#brew install vim
-#brew postinstall perl
-#brew install cmake
-
-alias python="/usr/bin/env python3"
-
-#Install binary from source (in this case, Node v8)
-curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
-
-#Now that we have the package, let's install it
-sudo yum -y install nodejs
-sudo yum install gcc-c++ make;
-sudo yum install iptables-services
-
-#Let's go back to the mail directory
+yes | brew install node
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+sudo iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4000
 cd ..
+
