87448e23975ec13411712d83fcf1a4fcdbde2ad7
Jose Barrios <github@barrios.io>
2019-05-30 21:30:14 -0700

    Ubuntu setup update
    Signed-off-by: Jose Barrios <github@barrios.io>

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,10 +1,11 @@
-yes | brew install node
+yes | brew install node@10 #LTS EOL: 2021-04-01
+echo 'export PATH="/home/linuxbrew/.linuxbrew/opt/node@10/bin:$PATH"' >> ~/.bash_profile
+export LDFLAGS="-L/home/linuxbrew/.linuxbrew/opt/node@10/lib"
+export CPPFLAGS="-I/home/linuxbrew/.linuxbrew/opt/node@10/include"
+
 yes | brew install cmake
 yes | brew install python2
 yes | brew install gcc
-
-# Install n node version manager
-yes | curl -L https://git.io/n-install | bash
 
 # Port forwarding
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
