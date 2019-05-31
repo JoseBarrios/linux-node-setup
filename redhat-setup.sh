10c329e62437fea2bd1b978c99d865aabc04bd24
Jose Barrios <github@barrios.io>
2019-05-30 19:15:28 -0700

    Ubuntu setup
    Signed-off-by: Jose Barrios <github@barrios.io>

--- redhat-setup.sh
+++
@@ -1,5 +0,0 @@
-yes | brew install node
-sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
-sudo iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4000
-cd ..
-
