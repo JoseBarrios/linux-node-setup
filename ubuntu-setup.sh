0df04de61bd371076c7a4f0111451a30b6bf70f7
Jose Barrios <github@barrios.io>
2019-05-30 20:26:27 -0700

    Installs n
    Signed-off-by: Jose Barrios <github@barrios.io>

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -3,7 +3,12 @@ yes | brew install cmake
 yes | brew install python2
 yes | brew install gcc
 
+# Install n node version manager
+yes | curl -L https://git.io/n-install | bash
+
+# Port forwarding
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
 sudo iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4000
+
 cd ..
 
